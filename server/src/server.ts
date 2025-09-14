fe84a0810200081ab085a27507c79a6066c3d724
Adiii581 <ac.aditya123@gmail.com>
2025-09-13 21:18:23 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ const server = createServer(app);
 
 // This is the important part for Render.
 // It uses the PORT from the environment, or defaults to 4002 for local testing.
-const PORT = parseInt(process.env.PORT || '4002');
+const PORT = parseInt(process.env.PORT || '10000');
 
 const peerServer = ExpressPeerServer(server, {
     path: '/myapp' // This path must match your client configuration
